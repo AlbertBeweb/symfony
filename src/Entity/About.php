65c6424b4bb9960caaa2cc2081da6d2dab7b6060
AlbertBeweb <contact@albert-nicolas.fr>
2019-11-05 16:28:40 +0100

    php bin/console make:entity

---
+++ src/Entity/About.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\AboutRepository")
+ */
+class About
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $descripion;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescripion(): ?string
+    {
+        return $this->descripion;
+    }
+
+    public function setDescripion(string $descripion): self
+    {
+        $this->descripion = $descripion;
+
+        return $this;
+    }
+}
