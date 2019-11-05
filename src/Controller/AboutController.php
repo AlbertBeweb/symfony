a005769ffd3435360e2213e38ed3279019a0d333
AlbertBeweb <contact@albert-nicolas.fr>
2019-11-05 16:08:40 +0100

    Modification Active item Navbar

--- src/Controller/AboutController.php
+++ src/Controller/AboutController.php
@@ -3,7 +3,6 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Twig\Environment;
 
 class AboutController extends AbstractController
 {
